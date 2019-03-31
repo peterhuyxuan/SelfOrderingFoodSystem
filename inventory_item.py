f950ca8d012ccacab15e9fd5d17a0064f4cb5a9f
SkitzAvernal <shwethway98@gmail.com>
2019-03-31 19:44:41 +1100

    Completed some work on inventory_item.py

---
+++ inventory_item.py
@@ -0,0 +1,45 @@
+from abc import ABC
+
+class inventoryItem(ABC):
+
+    def __init__(self, name, quantity, price, ivid):
+        self._name = name
+        self._quantity = quantity
+        self._price = price
+        self._ivid = ivid
+        
+    def calc_price(self, item_count):
+        return item_count * self._price
+        
+    @property
+    def name(self):
+        return self._name
+        
+    @property
+    def quantity(self):
+        return self._quantity
+            
+    @property
+    def price(self):
+        return self._price
+            
+    @property
+    def ivid(self):
+        return self._ivid
+        
+    def __str__(self):
+        return f'Item <{self.name}, {self.quantity}, {self.price}, {self.ivid}>'
+        
+class Sides:
+    def __init__(self, name, quantity, price, ivid):
+        super().__init__(name, quantity, price, ivid)
+    
+    def __str__(self):
+        return f'Item <{self.name}, {self.quantity}g, {self.price}, {self.ivid}>'
+        
+class Drink:
+    def __init__(self, name, quantity, price, ivid):
+        super().__init__(name, quantity, price, ivid)
+    
+    def __str__(self):
+        return f'Item <{self.name}, {self.quantity}ml, {self.price}, {self.ivid}>'
