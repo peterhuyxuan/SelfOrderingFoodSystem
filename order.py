e240ce8197b9f2d96c7beb28d0d42cbac9a913ea
Peter Nguyen <z5061984@ad.unsw.edu.au>
2019-04-05 10:59:09 +1100

    latest iteration of order.py and restaurant.py

--- order.py
+++ order.py
@@ -1,53 +1,32 @@
-from menu_item import  Side, Drink
-from main import Main
+from abc import ABC, abstractmethod,abstractproperty
+from menu_item import Bun, Patty, OtherIngredient, Side, Drink
+
 class Order():
 
 	def __init__(self):
-<<<<<<< HEAD
 		self._order_done = False
-=======
-            """
-            items should be a dicitonary
-            use name of the item as key
-            you dont need _num_mains and _categoriesgories
-            """ 
-		self._status = ("Preparing Order", "Ready for Pickup"(
->>>>>>> 46a083f02c2c5757018ca85b8288489c139a211a
 		self._total_price = 0
-		self._items = []
+		self._items = {}
 		Order._id += 1
 		self._id = Order._id
 		
-        def add_item(self, item, qty):
+	def add_item(self, name, categories, price, component, qty):
         self._check_name_exists(name)
-            if isinstance(item, Main):
-            """
-            use isinstance like this
-            """
-			item = Bun(name, price, component, qty)
-		elif isintance(categories, patties):
-			item = Patty(name, price, component, qty)
-		elif isintance(categories, side):
-			item = Side(name, price, component, qty)
-		elif isintance(categories, drink):
+		if isinstance(item, Main):
+		    #
+		elif isinstance(Item, side):
+			item = Side(name, price, Item, qty)
+		elif isinstance(categories, drink):
 			item = Drink(name, price, component, qty)
-		elif isintance(categories, other):
-			item = OtherIngredient(name, price, component, qty)
+
 		self._total_price += qty * price
         self._items.append(item)
 	
-	def remove_item(self, item):
-        """
-        change item to item_name which is a string
-        iterate through the dictionary to find key has save value
-        remove that entry
-        """
+	def remove_item(self, item, price):
         if item in self._items:
             if isinstance(item, buns):
-				self._num_mains -= 1
 				self._total_price -= price
 			elif isintance(item, patties):
-				self._num_mains -= 1
 				self._total_price -= price
 			else:
 				self._total_price -= price
@@ -75,7 +54,6 @@ class Order():
         	
 	# properties
 	@property
-<<<<<<< HEAD
 	def order_done(self):
 		return self._order_done
 	
@@ -90,19 +68,3 @@ class Order():
 	@property
 	def id(self):
         return self._id
-=======
-	def status(self):
-		return self._status
-	
-        @property
-	def total_price(self):
-		return self._total_price
-        
-        @property
-	def items(self):
-		return self._items
-		
-        @property
-	def num_mains(self):
-		return self._num_mains
->>>>>>> 46a083f02c2c5757018ca85b8288489c139a211a
