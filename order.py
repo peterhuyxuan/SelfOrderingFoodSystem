5cc343d5ff7d053790984f46ea7e48c366bdead3
Peter Nguyen <z5061984@ad.unsw.edu.au>
2019-04-05 11:59:56 +1100

    latest iteration of order.py and restaurant.py

--- order.py
+++ order.py
@@ -1,49 +1,49 @@
 from abc import ABC, abstractmethod,abstractproperty
-from menu_item import Bun, Patty, OtherIngredient, Side, Drink
+from main import Main
+from menu_item import Side, Drink
 
 class Order():
-
+    _id = -1
 	def __init__(self):
 		self._order_done = False
 		self._total_price = 0
 		self._items = {}
+		self._mains = []
 		Order._id += 1
 		self._id = Order._id
 		
-	def add_item(self, name, categories, price, component, qty):
+	def add_others(self, item, qty):
         self._check_name_exists(name)
-		if isinstance(item, Main):
-		    #
-		elif isinstance(Item, side):
-			item = Side(name, price, Item, qty)
-		elif isinstance(categories, drink):
-			item = Drink(name, price, component, qty)
-
-		self._total_price += qty * price
-        self._items.append(item)
+		if isinstance(item, Side):
+		    self._items[item.name] = qty
+		elif isinstance(item, Drink):
+			self._items[item.name] = qty
+		self._total_price += qty * item.price
 	
-	def remove_item(self, item, price):
-        if item in self._items:
-            if isinstance(item, buns):
-				self._total_price -= price
-			elif isintance(item, patties):
-				self._total_price -= price
-			else:
-				self._total_price -= price
-			self._items.remove(item)
+	def update_other(self, item, qty): 
+	    try:
+	        self._items[item.name] = qty
+	    except KeyError:
+	        print(f"{item.name} is not in the order")
+	        
+	def add_main(self, main):
+	    self._mains.append(main)
+	    self._price += main.price
+	
+	def remove_other(self, item):
+        try:
+            qty = self._items.pop(item.name)
+        except KeyError:
+	        print(f"{item.name} is not in the order")
+        else:
+            self._total_price -= qty * item.price
+            
+    def add_main(self, main):
+	    self._mains.append(main)
+	    self._price += main.price
 			
 	def mark_finished(self):
 		self._order_done = True
-	
-	def order_removal(self, order_id):
-		for id in self._id
-			if id == order_id
-				self._order_done = False
-				self._total_price = 0
-				new_order._items = []
-				Order._id -= 1
-				new_order._id = Order._id
-				break
     
     def order_checkout(self, order_id):
         self._order_done = True
--- restaurant.py
+++ restaurant.py
@@ -1,27 +1,47 @@
 from abc import ABC, abstractmethod,abstractproperty
 from order import Order
 from menu import Menu
+from main import Main
+from menu_item import MenuItem
 from inventory import inventory
 
 class Restaurant():
 	def __init__(self):
 		self._orders = []
-		self._inventory_system = []
+		self._inventory = []
 		self._menu = Menu()
 		
-	def make_order(self, menu_item, name, price, component, qty):
-		new_order = Order()
-		new_order._items = add_item(name, menu_item, price, component, qty) #edit according to new order
-	
-	def change_order_status(self):
-		mark_finished()
+	def add_order(self, order):
+	    self._orders.append(order)
+
+    def change_order_status(self, order_id):
+		order = self.get_order(order_id)
+		order.mark_finished()
 	
 	def remove_order(self, order_id):
-		order_removal(order_id)
+		order = self.get_order(order_id)  
+		if order is not None:
+		    self._orders.remove(order)
 	
 	def checkout(self, order_id):
-	    order_checkout(order_id)
+	    order = self.get_order(order_id)
+	    order.mark_finished()
+	    for item_name, qty  in order.items:
+	        if isinstance(item, Main):
+	            for component_id, qty in item.component.items():
+	                self.consume_stock(component_id, qty)
+            if isinstance(item, MenuItem):
+                self.consume_stock(component_it, qty)
+	    
 	    # reduce_inventory
+	    self.remove_order(order_id)
+
+    def get_order(self, order_id):
+        for order in self._orders:
+            if order_id == order.id:
+                return order
+        return None
+            
 		
 	# properties
 	@property
