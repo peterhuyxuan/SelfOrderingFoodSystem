7bd55d16c71330d6ff3e32fe3070e8f136fea851
Peter Nguyen <z5061984@ad.unsw.edu.au>
2019-04-18 04:12:21 +1000

    Finished HTML Template for Menu and Order

--- routes.py
+++ routes.py
@@ -7,8 +7,22 @@ def base():
 
 @app.route('/menu')
 def menu():
+    #if request.form["checkout"] == "Checkout":
+        # Need to redirect menu to order when submitting the form
+    #    return redirect(url_for('order'))
     return render_template('menu.html')
 
 @app.route('/order')
 def order():
+    #if request.form["payment"] == "Pay by Cash" or request.form["payment"] == "Pay by Card":
+        # Need to redirect order to confirm when pressing payment choice
+    #    return redirect(url_for('confirmation'))
     return render_template('order.html')
+    
+@app.route('/confirmation')
+def confirmation():
+    return render_template('confirmation.html')
+    
+@app.route('/order_staff')
+def staff():
+    return render_template('order_staff.html')
